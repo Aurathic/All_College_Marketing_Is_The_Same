85d993020d13649e88d5a5faee19887594ecb358
Adam <39442291+Aurathic@users.noreply.github.com>
2020-01-05 14:45:35 -0500

    Convert college array to JSON

--- main.js
+++ main.js
@@ -1,32 +1,108 @@
-example_array = ["CMU_A\n\
-\n\
-CMU_B\n\
-\n\
-CMU_C",
-"Colby_A\n\
-\n \
-Colby_B\n\
-\n\
-Colby_C",
-"Cornell_A\n\
-\n\
-Cornell_B", 
-"Pomona_A"];
-
 college_quotes = [];
 
-async function getCollegeFile(college_name) {
-	fetch("./colleges/" + college_name + ".txt")
+async function getCollegeFile(college) {
+	college_file_name = college["file_name"];
+	fetch("./colleges/" + college_file_name + ".txt")
 	.then((response) => {
 		return response.text();
 	}).then((text) => {
-		college_quotes.push(text.split("\n\n"));
-	}).catch(() => {
-		college_quotes.push(randElement(example_array).split("\n\n"));
+		college_quotes.push(
+		{
+			"name": college["name"],
+			"quotes": splitQuotes(text)
+		});
+	}).catch((error) => {
+		console.log(example_json[college_file_name])
+		college_quotes.push(
+		{
+			"name": college["name"],
+			"quotes": college["name"]
+		});
 	});
 }
 
-colleges = ["carnegie_mellon", "colby", "colgate", "cornell", "dartmouth", "georgetown", "grinell", "harvard", "haverford", "holy_cross", "macalester", "middlebury", "mit", "mount_holyoke", "pomona", "santa_clara", "university_of_california_davis", "university_of_chicago", "university_of_michigan", "university_of_pennsylvania"]
+function splitQuotes(quotes) {
+	return quotes.split("\n\n");
+}
+
+colleges = [
+	{
+		"name": "Carnegie Mellon University",
+		"file_name": "carnegie_mellon"
+	},
+	{	
+		"name": "Colby College",
+		"file_name": "colby"
+	},
+	{
+		"name": "Colgate University",
+		"file_name": "colgate"
+	},
+	{
+		"name": "Cornell University",
+		"file_name": "cornell"
+	},
+	{
+		"name": "Dartmouth University",
+		"file_name": "dartmouth"
+	},
+	{
+		"name": "Georgetown University",
+		"file_name": "georgetown"
+	},
+	{
+		"name": "Grinell College",
+		"file_name": "grinell"
+	},	
+	{
+		"name": "Harvard University",
+		"file_name": "harvard"
+	},
+	{
+		"name": "Haverford College",
+		"file_name": "haverford"
+	},
+	{
+		"name": "College of the Holy Cross",
+		"file_name": "holy_cross"
+	},
+	{
+		"name": "Macalester College",
+		"file_name": "macalester"
+	},
+	{
+		"name": "Mount Holyoke College",
+		"file_name": "mount_holyoke"
+	},
+	{
+		"name": "Middlebury College",
+		"file_name": "middlebury"
+	},
+	{
+		"name": "Pomona College",
+		"file_name": "pomona"
+	},
+	{
+		"name": "Santa Clara University",
+		"file_name": "santa_clara"
+	},
+	{
+		"name": "University of California Davis",
+		"file_name": "university_of_california_davis"
+	},
+	{
+		"name": "University of Chicago",
+		"file_name": "university_of_chicago"
+	},
+	{
+		"name": "University of Michigan",
+		"file_name": "university_of_michigan"
+	},
+	{
+		"name": "University of Pennsylvania",
+		"file_name": "university_of_pennsylvania"
+	}
+];
 
 function collegeA() {
 	college_quotes = [];
