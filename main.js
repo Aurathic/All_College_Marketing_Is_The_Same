ae2f70e2b0d7a601b0ad1360440027853a275748
Adam <39442291+Aurathic@users.noreply.github.com>
2020-01-05 13:33:02 -0500

    Rewrite getCollegeFile to use 'then', add catch, add randElement

--- main.js
+++ main.js
@@ -16,9 +16,14 @@ Cornell_B",
 college_quotes = [];
 
 async function getCollegeFile(college_name) {
-	resp = await fetch("./colleges/" + college_name + ".txt");
-	file = await resp.text()
-	college_quotes.push(file.split("\n\n"));
+	fetch("./colleges/" + college_name + ".txt")
+	.then((response) => {
+		return response.text();
+	}).then((text) => {
+		college_quotes.push(text.split("\n\n"));
+	}).catch(() => {
+		college_quotes.push(randElement(example_array).split("\n\n"));
+	});
 }
 
 colleges = ["carnegie_mellon", "colby", "colgate", "cornell", "dartmouth", "georgetown", "grinell", "harvard", "haverford", "holy_cross", "macalester", "middlebury", "mit", "mount_holyoke", "pomona", "santa_clara", "university_of_california_davis", "university_of_chicago", "university_of_michigan", "university_of_pennsylvania"]
@@ -29,8 +34,12 @@ function collegeA() {
 	console.log(college_quotes);
 }
 
+function randElement(array) {
+	return array[Math.floor(Math.random()*array.length)];
+}
+
 function collegeB() {
-	for(let i = 0; i < 100; i++) {
-		console.log(college_quotes[Math.floor(Math.random()*college_quotes.length)])
+	for(let i = 0; i < 20; i++) {
+		console.log(randElement(college_quotes));
 	}
 }
