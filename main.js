8ccd14b00def9995e0c71789aa1ef29a4e842761
Adam <39442291+Aurathic@users.noreply.github.com>
2020-01-05 13:06:45 -0500

    Simplify file fetching, begin writing quote selection code

--- main.js
+++ main.js
@@ -18,18 +18,19 @@ college_quotes = [];
 async function getCollegeFile(college_name) {
 	resp = await fetch("./colleges/" + college_name + ".txt");
 	file = await resp.text()
-	college_quotes.push(file);
-}
-
-function splitQuotes(quotes) {
-	return quotes.split("\n\n");
+	college_quotes.push(file.split("\n\n"));
 }
 
 colleges = ["carnegie_mellon", "colby", "colgate", "cornell", "dartmouth", "georgetown", "grinell", "harvard", "haverford", "holy_cross", "macalester", "middlebury", "mit", "mount_holyoke", "pomona", "santa_clara", "university_of_california_davis", "university_of_chicago", "university_of_michigan", "university_of_pennsylvania"]
 
 function collegeA() {
 	college_quotes = [];
-    colleges.forEach(getCollegeFile);
-    console.log(college_quotes);
-    
+	colleges.forEach(getCollegeFile);
+	console.log(college_quotes);
+}
+
+function collegeB() {
+	for(let i = 0; i < 100; i++) {
+		console.log(college_quotes[Math.floor(Math.random()*college_quotes.length)])
+	}
 }
