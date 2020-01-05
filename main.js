183d40563b7c73c8d5057d03fdfe475d240f09ce
Adam <39442291+Aurathic@users.noreply.github.com>
2020-01-05 12:51:12 -0500

    Modify getCollegeFile to reference external variable :(

--- main.js
+++ main.js
@@ -1,23 +1,24 @@
-example_array = ["CMU_A\n \
-\n \
-CMU_B\n \
-\n \
+example_array = ["CMU_A\n\
+\n\
+CMU_B\n\
+\n\
 CMU_C",
-"Colby_A\n \
+"Colby_A\n\
 \n \
-Colby_B\n \
-\n \
+Colby_B\n\
+\n\
 Colby_C",
-"Cornell_A\n \
-\n \
+"Cornell_A\n\
+\n\
 Cornell_B", 
 "Pomona_A"];
 
+college_quotes = [];
+
 async function getCollegeFile(college_name) {
 	resp = await fetch("./colleges/" + college_name + ".txt");
-	file = await file.text()
-	console.log(file);
-	return file;
+	file = await resp.text()
+	college_quotes.push(file);
 }
 
 function splitQuotes(quotes) {
@@ -27,6 +28,8 @@ function splitQuotes(quotes) {
 colleges = ["carnegie_mellon", "colby", "colgate", "cornell", "dartmouth", "georgetown", "grinell", "harvard", "haverford", "holy_cross", "macalester", "middlebury", "mit", "mount_holyoke", "pomona", "santa_clara", "university_of_california_davis", "university_of_chicago", "university_of_michigan", "university_of_pennsylvania"]
 
 function collegeA() {
-    texts = colleges.map(getCollegeFile)
-    console.log(texts);
+	college_quotes = [];
+    colleges.forEach(getCollegeFile);
+    console.log(college_quotes);
+    
 }
