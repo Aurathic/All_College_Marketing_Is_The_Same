4c4fd66ef9672044da2694a2e8f7b1eba397877b
Adam <39442291+Aurathic@users.noreply.github.com>
2020-01-04 23:11:27 -0500

    Rewrite file access code to use `fetch`, add splitQuotes, add sample quote strings

--- main.js
+++ main.js
@@ -1,28 +1,36 @@
-//Adapted from https://stackoverflow.com/questions/14446447/how-to-read-a-local-text-file
-function readTextFile(file)
-{
-    var xhr = new XMLHttpRequest();
-	xhr.responseType = 'text';
+example_array = ["CMU_A\n \
+\n \
+CMU_B\n \
+\n \
+CMU_C",
+"Colby_A\n \
+\n \
+Colby_B\n \
+\n \
+Colby_C",
+"Cornell_A\n \
+\n \
+Cornell_B", 
+"Pomona_A"];
 
-	xhr.onload = function () {
-  	  if (xhr.readyState === xhr.DONE) {
-    	    if (xhr.status === 200) {
-     	       console.log(xhr.response);
-     	       return xhr.responseText;
-   	     }
- 	   }
-	};
-	xhr.open("GET", file);
-	xhr.send(null);
+async function getCollegeFile(college_name) {
+     file = await fetch("./colleges/" + college_name + ".txt");
+     console.log(file);
+     return file;
 }
 
-function getCollegeFile(college_name) {
-    return readTextFile("./colleges/" + college_name + ".txt")
+function splitQuotes(quotes) {
+	return quotes.split("\n\n");
 }
 
 colleges = ["carnegie_mellon", "colby", "colgate", "cornell", "dartmouth", "georgetown", "grinell", "harvard", "haverford", "holy_cross", "macalester", "middlebury", "mit", "mount_holyoke", "pomona", "santa_clara", "university_of_california_davis", "university_of_chicago", "university_of_michigan", "university_of_pennsylvania"]
 
+texts = colleges.map(getCollegeFile)
+console.log(texts);
+
+
+
+
 function collegeA() {
-    texts = colleges.map(getCollegeFile)
-    console.log(texts);
+    
 }
